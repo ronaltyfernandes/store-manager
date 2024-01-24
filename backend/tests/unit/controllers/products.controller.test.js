bff22c7dc54c9d6b52f71c85b0a65f7dc29e21cf
ronalty fernandes <ronaltyfernandess@gmail.com>
2024-01-23 23:18:38 -0300

    lint

--- backend/tests/unit/controllers/products.controller.test.js
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -4,9 +4,9 @@ const sinonChai = require('sinon-chai');
 const { productController } = require('../../../src/controllers/index');
 const { productServices } = require('../../../src/services/index');
 const { controllersMocks } = require('../mocks/index.mock');
-const { message, statusNumbers } = require('../../../src/controllers/statusMensages');
+const { statusNumbers } = require('../../../src/controllers/statusMensages');
 
-const { findAllControllerMock } = controllersMocks;
+const { findAllControllerMock, findByIdControllerMock, insertControllerMock } = controllersMocks;
 
 const { expect } = chai;
 
@@ -30,6 +30,78 @@ describe('Realizando testes - PRODUCT SERVICES:', function () {
     expect(response.status).to.have.been.calledWith(statusNumbers.ok);
     expect(response.json).to.have.been.calledWith(findAllControllerMock.data);
   });
+
+  it('Recuperando a lista products com um ID especifico', async function () {
+    sinon.stub(productServices, 'findById').returns(findByIdControllerMock);
+
+    const request = {
+      params: { id: 1 },
+      body: { },
+    };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productController.findById(request, response);
+
+    expect(response.status).to.have.been.calledWith(statusNumbers.ok);
+    expect(response.json).to.have.been.calledWith(findByIdControllerMock.data);
+  });
+
+  it('INSERINDO a lista products', async function () {
+    sinon.stub(productServices, 'insert').returns(insertControllerMock);
+
+    const request = {
+      params: { },
+      body: { name: 'exemploX' },
+    };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productController.insert(request, response);
+
+    expect(response.status).to.have.been.calledWith(statusNumbers.postOk);
+    expect(response.json).to.have.been.calledWith(insertControllerMock.data);
+  });
+
+  it('MODIFICANDO a lista products com PUT', async function () {
+    sinon.stub(productServices, 'insert').returns(findByIdControllerMock);
+
+    const request = {
+      params: { id: 1 },
+      body: { name: 'exemploX' },
+    };
+    const response = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await productController.insert(request, response);
+
+    expect(response.status).to.have.been.calledWith(statusNumbers.ok);
+    expect(response.json).to.have.been.calledWith(findByIdControllerMock.data);
+  });
+
+  // it('MODIFICANDO a lista products com PUT', async function () {
+  //   sinon.stub(productServices, 'insert').returns(findByIdControllerMock);
+
+  //   const request = {
+  //     params: { id: 1 },
+  //     body: { name: 'exemploX' },
+  //   };
+  //   const response = {
+  //     status: sinon.stub().returnsThis(),
+  //     json: sinon.stub(),
+  //   };
+
+  //   await productController.insert(request, response);
+
+  //   expect(response.status).to.have.been.calledWith(statusNumbers.ok);
+  //   expect(response.json).to.have.been.calledWith(findByIdControllerMock.data);
+  // });
 
   afterEach(function () {
     sinon.restore();
--- backend/tests/unit/mocks/controllers.mock.js
+++ backend/tests/unit/mocks/controllers.mock.js
@@ -1,23 +1,23 @@
-const { message, statusNumbers } = require('../../../src/controllers/statusMensages');
-const { productAllMock } = require('./products.mock');
+const { statusNumbers } = require('../../../src/controllers/statusMensages');
+const { productAllMock, productByIdMock } = require('./products.mock');
 
 const findAllControllerMock = {
   status: statusNumbers.ok,
   data: productAllMock,
 };
-const travelFromServiceInvalidValue = {
-  status: 'INVALID_VALUE',
-  data: { message: 'message' },
+const findByIdControllerMock = {
+  status: statusNumbers.ok,
+  data: productByIdMock[0],
 };
-const travelFromServiceNotFound = {
-  status: 'NOT_FOUND',
-  data: { message: 'message' },
+const insertControllerMock = {
+  status: statusNumbers.postOk,
+  data: productByIdMock[0],
 };
-const travelFromServiceConflict = {
-  status: 'CONFLICT',
-  data: { message: 'message' },
-};
+// const travelFromServiceConflict = {
+//   status: 'CONFLICT',
+//   data: { message: 'message' },
+// };
 
 module.exports = {
-  findAllControllerMock,
+  findAllControllerMock, findByIdControllerMock, insertControllerMock,
 };
